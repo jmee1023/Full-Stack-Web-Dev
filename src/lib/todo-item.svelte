fc8ce1d91221f7f9d2c54b1e02c11da48fad9ea2
jmee1023 <jmmee@uvm.edu>
2022-01-05 14:39:49 +0000

    Home page edited

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,14 @@
+<script>
+import { text } from "svelte/internal";
+
+</script>
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="" />
+        <button aria-label="Mark done/not done">Done/NotDone</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text"
+    </form>
+</div>

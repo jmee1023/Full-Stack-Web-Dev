14bd65844daacd9507bcad60d410e18e67924f39
jmee1023 <jmmee@uvm.edu>
2022-06-04 16:39:44 +0000

    Develop HTML Structure.

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="" />
+        <button aria-label="Mark done/not Done">Done/NotDone</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="Save ToDo">Save</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="Delete Todo">Delete</button>
+    </form>
+</div>

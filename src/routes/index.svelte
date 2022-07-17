8e420fbeb305e3fbb38388657492db09d39f4c3f
jmee1023 <jmee0823@gmail.com>
2022-07-17 23:10:51 +0000

    Creating Todos array and CRUD endpoints

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,29 @@
-<script>
+<script context="module" lang="ts">
+    import type { Load } from "@sveltejs/kit"
+    export const load: Load = async ({ fetch }) => {
+        const res = await fetch("/tddos.json");
+
+        if (res.ok) {
+            const todos = await res.json();
+        return {
+            props: { todos }
+        }
+        }
+
+
+        const { message } = await res.json();
+        return{
+            error: new Error(message)
+        }
+    };
+</script>
+
+<script lang="ts">
     
     import TodoItem from "$lib/todo-item.svelte";
+
+    export let todos;
+
     const title = "Todo";
 </script>
 
@@ -43,7 +66,7 @@
 <div class="todos"> 
 <h1>{title}</h1>
 
-<form action="" method="" class="new"> 
+<form action="/todos.json" method="post" class="new"> 
     <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo" />
 </form>
 
